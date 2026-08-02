e3d6bbb4d7eb62afbf835dbc9087dd2d29e5dae9
Dhruv Pandey <dhruvpandey445@gmail.com>
2026-08-03 00:54:51 +0530

    Added volume

--- ui/charts.py
+++ ui/charts.py
@@ -1,8 +1,16 @@
 import plotly.graph_objects as go
-from indicators.moving_average import calculate_ma
+from plotly.subplots import make_subplots
+import numpy as np
+
 
 def plot_price_chart(df,ticker,show_ma20,show_ma50,show_ma100):
-    fig = go.Figure()
+    fig = make_subplots(
+          rows = 2,
+          cols = 1,
+          shared_xaxes= True,
+          vertical_spacing= 0.05,
+          row_heights=[0.65,0.35]
+    )
 
     fig.add_trace(
         go.Candlestick(
@@ -12,46 +20,81 @@ def plot_price_chart(df,ticker,show_ma20,show_ma50,show_ma100):
             high = df["High"],
             low = df["Low"],
             name = "Candlestick"
-        )
+        ),
+        row = 1,
+        col = 1
     )
     if show_ma20:
-        period = 20
         fig.add_trace(
             go.Scatter(
                     x = df.index,
                     y = df[f"MA20"],
                     mode = "lines",
                     name = f"Moving average 20"
-                )
+                ),
+                row = 1,
+                col = 1
         )
 
     if show_ma50:
-            period = 50
             fig.add_trace(
                 go.Scatter(
                         x = df.index,
                         y = df[f"MA50"],
                         mode = "lines",
                         name = f"Moving average 50"
-                    )
+                    ),
+                    row = 1,
+                    col = 1
             )
 
     if show_ma100:
-            period = 100
             fig.add_trace(
                 go.Scatter(
                         x = df.index,
                         y = df[f"MA100"],
                         mode = "lines",
                         name = f"Moving average 100"
-                    )
+                    ),
+                    row = 1,
+                    col = 1
             )
+    colors = np.where(
+      df["Close"] >= df["Open"],
+      "green",
+      "red"
+    )
+    fig.add_trace(
+          go.Bar(
+                x = df.index,
+                y = df["Volume"],
+                name = "Volume",
+                marker_color = colors
+          ),
+          row = 2,
+          col = 1
+    )
 
     fig.update_layout(
+        height = 600,
+        xaxis_rangeslider_visible = False,
         title = f"{ticker} Stock Price",
-        xaxis_title="Date",
-        yaxis_title="Price",
         template="plotly_white"
+    )
+    fig.update_yaxes(
+          title_text="Price",
+          row=1,
+          col=1
+    )
+    fig.update_yaxes(
+              title_text="Volume",
+              row=2,
+              col=1
+        )
+    fig.update_xaxes(
+          title_text = "Date",
+          row=2,
+          col=1
     )
 
     return fig
