819cefa4e755a327f6f0be17fe7c78e65dd9bfe0
Dhruv Pandey <dhruvpandey445@gmail.com>
2026-08-11 01:05:43 +0530

    Added volume indication

--- analysis/recommendation.py
+++ analysis/recommendation.py
@@ -1,3 +1,7 @@
+def calculate_volume(df):
+    df["Volume_MA20"] = df["Volume"].rolling(window=20).mean()
+    return df
+
 def generate_recommendation(df):
     latest = df.iloc[-1]
 
@@ -30,14 +34,38 @@ def generate_recommendation(df):
     elif latest["Close"] < latest["MA50"]:
         score -= 2
         reasons.append("Price is below the 50-day moving average")
+     
+    # Bollinger Band and 
+    if latest["close"] < latest["Lower"]:
+        score += 2
+        reasons.append("Price is below the lower bollinger band")
 
+    elif latest["close"] > latest["Higher"]:
+        score -= 2
+        reasons.append("Price is above the higher bollinger band")
+
+    # Volume
+    if latest["Volume"] > latest["Volume_MA20"]:
+        if latest["close"] > latest["Open"]:
+            score += 1
+            reasons.append("Volume confirms bullish price movement")
+    
+        elif latest["close"] < latest["Open"]:
+            score -= 1
+            reasons.append("High volume confirms bearish price movement")
+        
+    
     # Final recommendation
-    if score >= 4:
-        signal = "BUY"
+    if score >= 7:
+        signal = "Strongly Buy"
 
+    elif score <= -7:
+        signal = "Strongly Sell"
+
+    elif score >= 4:
+        signal = "Buy"
     elif score <= -4:
-        signal = "SELL"
-
+        signal = "Sell"
     else:
         signal = "HOLD"
 
--- indicators/bollinger_band.py
+++ indicators/bollinger_band.py
@@ -3,4 +3,5 @@ def bollinger_band(df):
     df["STD20"] = df["Close"].rolling(window=20).std()
     df["Upper"] = df["MA20"] + 2*(df["STD20"])
     df["Lower"] = df["MA20"] - 2*(df["STD20"])
+    df["BB_Width"] = (df["Upper"]-df["Lower"]) / df["MA20"]
     return df
